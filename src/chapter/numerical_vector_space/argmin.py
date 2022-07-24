252d2e467c3b15a2ef5a0454720d459841933b8e
--global <pacific.flying.squid+calamari@gmail.com>
2022-07-24 17:51:38 +0900

    一部加筆

--- src/chapter/numerical_vector_space/argmin.py
+++ src/chapter/numerical_vector_space/argmin.py
@@ -7,6 +7,7 @@ plt.style.use("sigproc")
 
 x = np.linspace(0, 8, 300)
 fig, ax = plt.subplots()
+ax.set(xticks=[0, np.pi, 2 * np.pi], xticklabels=["0", r"$\krez$", r"$2\krez$"])
 ax.plot(x, np.exp(-x))
 ax.plot(x, np.abs(np.sin(x)))
 fig.savefig(str(PurePath(__file__).parent / (PurePath(__file__).stem + ".pdf")))
---
+++ src/chapter/numerical_vector_space/proj2d.py
@@ -0,0 +1,27 @@
+from pathlib import PurePath
+
+import matplotlib.pyplot as plt
+import numpy as np
+from vector import Vector2D
+
+plt.style.use("sigproc")
+
+fig, ax = plt.subplots()
+ax.set(xlim=(-0.15, 1.15), ylim=(-0.15, 1.15))
+
+v = np.array((1, 0))
+x = np.array((np.cos(np.pi / 3), np.sin(np.pi / 3)))
+m = np.array((np.cos(np.pi / 3), 0))
+
+for p in (v, x, m):
+    ax.add_artist(Vector2D((0, 0), p))
+
+ax.add_artist(Vector2D(x, m, ls="dashed", arrowstyle="->"))
+
+ax.text(*v, r"$\vect{v}$", ha="left", va="center")
+ax.text(*x, r"$\vect{x}$", ha="left", va="bottom")
+ax.text(*(m + (0.05, 0.05)), r"$\vect{m}$", ha="left", va="bottom")
+ax.text(*((x + m) / 2), r"$\,\vect{m}-\vect{x}$", ha="left", va="center")
+
+ax.axis("square")
+fig.savefig(str(PurePath(__file__).parent / (PurePath(__file__).stem + ".pdf")))
