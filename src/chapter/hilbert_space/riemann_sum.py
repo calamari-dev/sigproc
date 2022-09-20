256fab27b48304032df3aa30b8592e30431bc5e7
--global <pacific.flying.squid+calamari@gmail.com>
2022-09-20 17:37:14 +0900

    一部修正

--- src/chapter/hilbert_space/riemann_sum.py
+++ src/chapter/hilbert_space/riemann_sum.py
@@ -5,16 +5,16 @@ import numpy as np
 
 plt.style.use("sigproc")
 
-n = 7
 x = np.linspace(0, 1, num=100)
-k = np.linspace(0, 1, num=n)
+n = np.linspace(0, 1, num=7)
 
 f = x**3 - 0.5 * x + 0.5
-g = k**3 - 0.5 * k + 0.5
+g = n**3 - 0.5 * n + 0.5
+g[-1] = g[-2]
 
 fig, ax = plt.subplots()
 ax.fill_between(
-    k, g, step="post", label=r"$\sum f(n\increment t)\increment t$", color="gray", lw=0
+    n, g, step="post", label=r"$\sum f(n\increment t)\increment t$", color="gray", lw=0
 )
 ax.plot(x, f, label=r"$f(t)$")
 ax.legend()
