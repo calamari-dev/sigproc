2b221706a4084166cd1175e08f588e247579003d
--global <pacific.flying.squid+calamari@gmail.com>
2022-09-22 22:35:14 +0900

    一様収束の例を修正

--- src/chapter/hilbert_space/func_convergence.py
+++ src/chapter/hilbert_space/func_convergence.py
@@ -5,14 +5,13 @@ import numpy as np
 
 plt.style.use("sigproc")
 
-x = np.linspace(0, 1, num=100)
+x = np.linspace(0, 1, num=300)
 
 fig, ax = plt.subplots()
 
 for i in range(1, 4):
-    ax.plot(x, x / i, label="$f_" + str(i) + "(x)$")
+    ax.plot(x, np.abs(np.sin(i * np.pi * x)) / i, label="$f_" + str(i) + "(x)$")
 
-ax.plot(x, x * 0, label=r"$\phi(x)$")
 ax.legend()
 ax.axis([0, 1, 0, 1])
 ax.axis("square")
