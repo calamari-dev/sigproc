ecc3b5bc5553098ee710cfa6f5a4d120d8f47530
--global <pacific.flying.squid+calamari@gmail.com>
2022-11-25 00:25:03 +0900

    音源を波音リツに差し替え

--- src/chapter/numerical_vector_space/frequency_domain.py
+++ src/chapter/numerical_vector_space/frequency_domain.py
@@ -7,18 +7,21 @@ from scipy.io import wavfile
 
 plt.style.use(["sigproc", "sigproc-wide"])
 
-filename = "common_voice_ja_23919598.wav"
+filename = "namine_ritsu.wav"
 samplerate, data = wavfile.read(str(PurePath(__file__).parent / filename))
 
-num = data.shape[0]
-duration = num / samplerate
-y = fft(data / 32767)
+duration = data.shape[0] / samplerate
+t = np.linspace(0, duration, num=data.shape[0])
+t = t[t < 0.1]
+data = data[0:len(t)] / 32767
+
+num = len(data)
+y = fft(data) / np.sqrt(num)
 x = fftfreq(num, 1 / samplerate)[:num // 2]
 x = x[x <= 5000]
 
 fig, ax = plt.subplots()
 ax.set_xlabel("周波数 [Hz]")
 ax.set_yscale("log")
-ax.plot(x, np.abs(y[0:len(x)]) / np.sqrt(num), lw=0.4)
-ax.set_yticks([1e-3, 1e-1], minor=False)
+ax.plot(x, np.abs(y[0:len(x)]), lw=0.4)
 fig.savefig(str(PurePath(__file__).parent / (PurePath(__file__).stem + ".pdf")))
--- src/chapter/numerical_vector_space/time_domain.py
+++ src/chapter/numerical_vector_space/time_domain.py
@@ -6,13 +6,15 @@ from scipy.io import wavfile
 
 plt.style.use(["sigproc", "sigproc-wide"])
 
-filename = "common_voice_ja_23919598.wav"
+filename = "namine_ritsu.wav"
 samplerate, data = wavfile.read(str(PurePath(__file__).parent / filename))
 
 duration = data.shape[0] / samplerate
 t = np.linspace(0, duration, num=data.shape[0])
+t = t[t < 0.1]
+data = data[0:len(t)] / 32767
 
 fig, ax = plt.subplots()
-ax.plot(t, data / 32767, lw=0.4)
+ax.plot(t, data, lw=0.4)
 ax.set_xlabel("時刻 [s]")
 fig.savefig(str(PurePath(__file__).parent / (PurePath(__file__).stem + ".pdf")))
