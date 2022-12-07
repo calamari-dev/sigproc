bd441a1d7719747f3993c0d06c907eec5875e6bc
--global <pacific.flying.squid+calamari@gmail.com>
2022-12-07 11:02:16 +0900

    標本化の説明を追加

--- src/figure.py
+++ src/figure.py
@@ -4,12 +4,12 @@ from pathlib import Path
 if __name__ == "__main__":
     selfdir = Path(__file__).parent
 
-    for fig in selfdir.glob("chapter/**/figure/*.py"):
+    for fig in selfdir.glob("chapter/**/figures/*.py"):
         print("processing " + fig.name + " ...")
         cwd = fig.parent.as_posix()
         subprocess.run(["python", fig.name], cwd=cwd)
 
-    for tex in selfdir.glob("chapter/**/figure/*.tex"):
+    for tex in selfdir.glob("chapter/**/figures/*.tex"):
         print("processing " + tex.name + " ...")
         cwd = tex.parent.as_posix()
         subprocess.run(["lualatex", tex.stem], cwd=cwd, stdout=subprocess.DEVNULL)
