077cb5ac34285411819e76e2729d378c442ca8bc
calamari-dev <pacific.flying.squid+calamari@gmail.com>
2023-01-15 02:41:26 +0900

    画像を高解像度のものに差し替え

--- src/chapter/numerical_vector_space/figures/2ddft.py
+++ src/chapter/numerical_vector_space/figures/2ddft.py
@@ -2,16 +2,23 @@ from pathlib import PurePath
 
 import matplotlib.pyplot as plt
 from matplotlib.colors import LogNorm
-from scipy import fftpack
+from scipy import fft
 import numpy as np
 from PIL import Image
 
 plt.style.use(["sigproc", "sigproc-wide"])
 
 png = Image.open(str(PurePath(__file__).parent / "building.png"))
-amp = np.abs(fftpack.fft2(png)) / np.sqrt(png.width * png.height)
+amp = np.abs(fft.fftshift(fft.fft2(png, norm="ortho")))
 
-fig, ax = plt.subplots()
-img = ax.imshow(amp, norm=LogNorm(vmin=np.min(amp), vmax=np.max(amp)))
+mm = 1 / 25.4
+fig, ax = plt.subplots(figsize=(60 * mm, 45 * mm))
+img = ax.pcolormesh(
+    *np.mgrid[-531:532, -531:532],
+    amp,
+    norm=LogNorm(vmin=np.min(amp), vmax=np.max(amp)),
+    rasterized=True
+)
 fig.colorbar(img)
+ax.axis("square")
 fig.savefig(str(PurePath(__file__).parent / (PurePath(__file__).stem + ".pdf")))
