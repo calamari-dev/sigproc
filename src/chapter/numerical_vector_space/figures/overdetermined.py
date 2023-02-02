3caaebf97ebcff4f432d0759ec1e9385fead635a
calamari-dev <pacific.flying.squid+calamari@gmail.com>
2023-02-02 20:03:37 +0900

    最小2乗法について加筆

--- src/chapter/numerical_vector_space/figures/overdetermined.py
+++ src/chapter/numerical_vector_space/figures/overdetermined.py
@@ -9,11 +9,11 @@ plt.style.use("sigproc")
 xx, yy = np.mgrid[-5:5:10j, -5:5:10j]
 zz = (xx + 2 * yy) / 3
 
-e1 = np.array((4, 1, 2))
-e2 = np.array((0, 3, 2))
-pr = np.cross(e1, e2) / 2
-xm = np.array((-2, -2, -2))
-y1 = xm + pr
+v1 = np.array((4, 1, 2))
+v2 = np.array((0, 3, 2))
+v3 = np.cross(v1, v2) / 2
+ym = np.array((-2, -2, -2))
+y1 = ym + v3
 
 fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
 ax.set(zlim3d=(-3, 7))
@@ -23,15 +23,15 @@ for axis in (ax.xaxis, ax.yaxis, ax.zaxis):
 
 ax.plot_surface(xx, yy, zz, alpha=0.25)
 
-for e in (e1, e2, y1, xm):
+for e in (v1, v2, y1, ym):
     ax.add_artist(Vector3D((0, 0, 0), e))
 
-ax.add_artist(Vector3D(y1, xm, ls="dashed", arrowstyle="->"))
+ax.add_artist(Vector3D(y1, ym, ls="dashed", arrowstyle="->"))
 
-ax.text(*(0.5 * (y1 + xm)), r"$P\,$", ha="right", va="top")
-ax.text(*e1, r"$\vect{e}_1$", ha="left", va="center")
-ax.text(*e2, r"$\vect{e}_2$", ha="left", va="bottom")
-ax.text(*xm, r"$\mat{A}\vect{x}$", ha="right", va="top")
+ax.text(*(0.5 * (y1 + ym)), r"$P\,$", ha="right", va="top")
+ax.text(*v1, r"$\vect{v}_1$", ha="left", va="center")
+ax.text(*v2, r"$\vect{v}_2$", ha="left", va="bottom")
+ax.text(*ym, r"$\mat{A}\vect{x}$", ha="right", va="top")
 ax.text(*y1, r"$\vect{y}$", ha="right", va="bottom")
 
 fig.savefig(str(PurePath(__file__).parent / (PurePath(__file__).stem + ".pdf")))
