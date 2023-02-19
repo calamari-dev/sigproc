99686104568e3331ca894903f696b3f914361d3c
calamari-dev <pacific.flying.squid+calamari@gmail.com>
2023-02-19 14:28:48 +0900

    図を修正

--- src/chapter/numerical_vector_space/figures/regression.py
+++ src/chapter/numerical_vector_space/figures/regression.py
@@ -8,11 +8,12 @@ from constant import constants
 plt.style.use(["sigproc", "sigproc-small"])
 
 fig, ax = plt.subplots()
-x = np.linspace(0, 2, 20)
+n = np.linspace(0, 2, 20)
+x = np.linspace(0, 2, 100)
 csv = pd.read_csv(
     str(PurePath(__file__).parent / "regression.csv"), sep=",", header=None
 )
-ax.scatter(x, csv[0], label=r"$(x_i,y_i)$", linewidths=constants.scatter_linewidth)
+ax.scatter(n, csv[0], label=r"$(x_i,y_i)$", linewidths=constants.scatter_linewidth)
 ax.plot(x, x**2 - 1.5 * x + 0.5, label=r"$f(\vect{c};x)$")
 ax.legend()
 fig.savefig(str(PurePath(__file__).parent / (PurePath(__file__).stem + ".pdf")))
