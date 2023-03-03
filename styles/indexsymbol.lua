08dc754daa9d7dd91b24a3f910935108f8549456
calamari-dev <pacific.flying.squid+calamari@gmail.com>
2023-03-03 13:13:54 +0900

    一部修正

--- styles/indexsymbol.lua
+++ styles/indexsymbol.lua
@@ -1,16 +1,15 @@
-rawset(_G, "indexsymbol", {})
+local indexsymbol = {}
+local symboltable = {}
+local currentorder = 0
 
-do
-  local symboltable = {}
-  local currentorder = 0
-
-  indexsymbol.run = function(symbol)
-    if symboltable[symbol] == nil then
-      currentorder = currentorder + 1
-      symboltable[symbol] = currentorder
-    end
-
-    local order = string.format("%03d", symboltable[symbol])
-    tex.sprint("\\index{" .. order .. "@" .. symbol .. "}")
+indexsymbol.run = function(symbol)
+  if symboltable[symbol] == nil then
+    currentorder = currentorder + 1
+    symboltable[symbol] = currentorder
   end
+
+  local order = string.format("%03d", symboltable[symbol])
+  tex.sprint("\\index{" .. order .. "@" .. symbol .. "}")
 end
+
+return indexsymbol
