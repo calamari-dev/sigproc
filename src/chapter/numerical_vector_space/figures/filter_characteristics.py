53f8c4e8b176c07376374a9e98161095ace0bab8
calamari-dev <pacific.flying.squid+calamari@gmail.com>
2023-03-06 19:28:51 +0900

    図を訂正

--- src/chapter/numerical_vector_space/figures/filter_characteristics.py
+++ src/chapter/numerical_vector_space/figures/filter_characteristics.py
@@ -7,14 +7,14 @@ from scipy.fft import fft
 plt.style.use("sigproc")
 
 N = 32
-K = round(N / 2)
-n = np.linspace(0, K, num=K)
+K = N // 2 + 1
+n = np.linspace(0, K - 1, num=K)
 x = np.zeros(N)
-x[0:2] = 1 / 3
+x[0:3] = 1 / 3
 y = np.abs(fft(x, norm="ortho"))
 
 fig, ax = plt.subplots()
 ax.ticklabel_format(style="sci", axis="y")
-ax.stem(n, y[: len(n)], label=r"$\abs{\hat{h}[k]}$")
+ax.stem(n, y[: K], label=r"$\abs{\hat{h}[k]}$")
 ax.legend()
 fig.savefig(str(PurePath(__file__).parent / (PurePath(__file__).stem + ".pdf")))
