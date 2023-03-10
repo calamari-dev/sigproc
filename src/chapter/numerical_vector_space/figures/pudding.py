debd49fb9cdb2ba76b9b3ffe038f9aa0c4da4cf0
calamari-dev <pacific.flying.squid+calamari@gmail.com>
2023-03-10 12:59:29 +0900

    2次元のエイリアシングを修正

--- src/chapter/numerical_vector_space/figures/pudding.py
+++ src/chapter/numerical_vector_space/figures/pudding.py
@@ -4,13 +4,13 @@ import matplotlib.pyplot as plt
 import numpy as np
 from PIL import Image
 
-plt.style.use(["sigproc", "sigproc-wide"])
+plt.style.use("sigproc")
 
 png = Image.open(str(PurePath(__file__).parent / "pudding.png"))
 mat = np.array(png.rotate(-90)).T / (2**16 - 1)
 
 mm = 1 / 25.4
-fig, ax = plt.subplots(figsize=(60 * mm, 45 * mm))
+fig, ax = plt.subplots(figsize=(55 * mm, 40 * mm))
 img = ax.pcolormesh(mat, rasterized=True)
 fig.colorbar(img)
 ax.axis("square")
--- src/chapter/numerical_vector_space/figures/pudding_shift.py
+++ src/chapter/numerical_vector_space/figures/pudding_shift.py
@@ -10,12 +10,12 @@ plt.style.use("sigproc")
 
 png = Image.open(str(PurePath(__file__).parent / "pudding.png"))
 mat = np.array(png.rotate(-90)).T / (2**16 - 1)
-amp = np.abs(fft.fft2(mat, norm="ortho"))
+amp = np.abs(fft.fftshift(fft.fft2(mat, norm="ortho")))
 
 mm = 1 / 25.4
-fig, ax = plt.subplots(figsize=(55 * mm, 40 * mm))
+fig, ax = plt.subplots(figsize=(60 * mm, 45 * mm))
 img = ax.pcolormesh(
-    *np.mgrid[0:1063, 0:1063],
+    *np.mgrid[-531:532, -531:532],
     amp,
     norm=LogNorm(vmin=np.min(amp), vmax=np.max(amp)),
     rasterized=True
