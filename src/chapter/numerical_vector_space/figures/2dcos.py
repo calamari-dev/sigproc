e7ba5c6ac325cb68a5bb3faab62d63277c25bd1d
calamari-dev <pacific.flying.squid+calamari@gmail.com>
2023-03-12 11:48:51 +0900

    2次元DFTの説明を加筆

--- src/chapter/numerical_vector_space/figures/2dcos.py
+++ src/chapter/numerical_vector_space/figures/2dcos.py
@@ -5,14 +5,12 @@ import numpy as np
 
 plt.style.use(["sigproc", "sigproc-wide"])
 
-t = np.linspace(-np.pi, np.pi, num=64)
+t = np.linspace(0, 2 * np.pi, num=64, endpoint=False)
 xx, yy = np.meshgrid(t, t)
 zz = np.cos(3 * xx + 4 * yy)
 
 mm = 1 / 25.4
 fig, ax = plt.subplots(figsize=(60 * mm, 45 * mm))
-ax.set(xticks=[-np.pi, 0, np.pi], xticklabels=[r"$-\krez$", r"$0$", r"$\krez$"])
-ax.set(yticks=[-np.pi, 0, np.pi], yticklabels=[r"$-\krez$", r"$0$", r"$\krez$"])
 img = ax.pcolormesh(xx, yy, zz, rasterized=True)
 fig.colorbar(img)
 ax.axis("square")
