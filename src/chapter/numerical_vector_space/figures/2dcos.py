439d73f258adfd5a8b6a3ee42d4fc27880160d28
calamari-dev <pacific.flying.squid+calamari@gmail.com>
2023-03-14 12:20:33 +0900

    画像を修正

--- src/chapter/numerical_vector_space/figures/2dcos.py
+++ src/chapter/numerical_vector_space/figures/2dcos.py
@@ -5,9 +5,9 @@ import numpy as np
 
 plt.style.use("sigproc")
 
-t = np.linspace(0, 2 * np.pi, num=64, endpoint=False)
+t = np.linspace(0, 2 * np.pi, num=50, endpoint=False)
 xx, yy = np.meshgrid(t, t)
-zz = np.cos(3 * xx + 4 * yy)
+zz = np.cos(3 * xx + 5 * yy)
 
 mm = 1 / 25.4
 fig, ax = plt.subplots(figsize=(55 * mm, 40 * mm))
--- src/chapter/numerical_vector_space/figures/2dcos_dft.py
+++ src/chapter/numerical_vector_space/figures/2dcos_dft.py
@@ -6,11 +6,11 @@ from scipy import fft
 
 plt.style.use("sigproc")
 
-n = np.linspace(-32, 32, num=64, endpoint=False)
-t = np.linspace(0, 2 * np.pi, num=64, endpoint=False)
+n = np.linspace(-25, 25, num=50, endpoint=False)
+t = np.linspace(0, 2 * np.pi, num=50, endpoint=False)
 uu, vv = np.meshgrid(n, n)
 xx, yy = np.meshgrid(t, t)
-zz = np.cos(3 * xx + 4 * yy)
+zz = np.cos(3 * xx + 5 * yy)
 amp = np.abs(fft.fftshift(fft.fft2(zz, norm="ortho")))
 
 mm = 1 / 25.4
