280e9e85b9b8b30090ae882a0e519067ef9b1315
calamari-dev <pacific.flying.squid+calamari@gmail.com>
2023-03-21 16:04:35 +0900

    一部修正

--- src/chapter/numerical_vector_space/figures/shah.py
+++ src/chapter/numerical_vector_space/figures/shah.py
@@ -12,7 +12,7 @@ d0[n % 5 == 0] = 1
 d1[n % 5 == 1] = 1
 
 fig, ax = plt.subplots()
-ax.stem(n, d0, label=r"$\delta[n]$")
-ax.stem(n, d1, label=r"$(\lagop\delta)[n]$", linefmt="--", markerfmt="x")
+ax.stem(n, d0, label=r"$\Shah[n]$")
+ax.stem(n, d1, label=r"$(\lagop\Shah)[n]$", linefmt="--", markerfmt="x")
 ax.legend()
 fig.savefig(str(PurePath(__file__).parent / (PurePath(__file__).stem + ".pdf")))
