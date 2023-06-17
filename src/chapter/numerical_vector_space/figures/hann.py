0c277450c54ace0c909d2ecf0c923fbad1864993
calamari-dev <pacific.flying.squid+calamari@gmail.com>
2023-06-17 18:22:38 +0900

    窓関数について追記

---
+++ src/chapter/numerical_vector_space/figures/hann.py
@@ -0,0 +1,22 @@
+from pathlib import PurePath
+
+import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib.colors import LogNorm
+from scipy import signal
+from scipy.io import wavfile
+
+plt.style.use("sigproc")
+
+filename = "phone_spectrogram.wav"
+samplerate, data = wavfile.read(str(PurePath(__file__).parent / filename))
+
+x = (data.astype(np.double) - 128) / 127
+f, t, spec = signal.spectrogram(
+    x, fs=samplerate, window="hann", nperseg=256, noverlap=255, nfft=len(x)
+)
+f = f / 1000
+
+fig, ax = plt.subplots()
+ax.pcolormesh(t, f, spec, norm=LogNorm(vmin=1e-10, vmax=np.max(spec)), rasterized=True)
+fig.savefig(str(PurePath(__file__).parent / (PurePath(__file__).stem + ".pdf")))
